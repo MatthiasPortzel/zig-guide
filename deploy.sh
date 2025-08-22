8c1fb0fc2112ab37720afd46c8431c55bd49bf41
Matthias Portzel <MatthiasPortzel@gmail.com>
2025-08-21 20:19:32 -0400

    Add ability to clone from static site

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,8 @@
 
     ./bin/zine release
 
+    git clone --bare . public/zig-guide.git
+    git --git-dir=public/zig-guide.git update-server-info
+
     wrangler deploy --assets=public --name=zig-guide --compatibility-date=2025-07-20
 )
